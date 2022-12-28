14fc7bd2453616003660d0d5b54aa8c835361b4d
Raul Burigo <raul.burigo@gmail.com>
2022-12-28 13:36:44 -0300

    publish flow

--- src/lib/Button/index.tsx
+++ src/lib/Button/index.tsx
@@ -1,27 +1,73 @@
-import { IButtonProps } from "./types";
 import { styled } from "../ThemeProvider/stitches.config";
+import {
+  DEFAULT_BUTTON_SIZE,
+  DEFAULT_BUTTON_VARIANT,
+  IButtonProps,
+} from './types'
 
 const StyledButton = styled('button', {
-    borderRadius: '$3',
-    cursor: 'pointer',
-    border: 'none',
-    fontWeight: 600,
-    lineHeight: '1.5em',
-  
-    '&:hover': {
-      opacity: 0.9,
+  borderRadius: '$3',
+  cursor: 'pointer',
+  border: 'none',
+  fontWeight: 600,
+  lineHeight: '1.5em',
+
+  '&:hover': {
+    opacity: 0.9,
+  },
+
+  '&:disabled': {
+    backgroundColor: '$gray6',
+    color: '$gray11',
+  },
+
+  variants: {
+    size: {
+      xs: {
+        fontSize: '$1',
+        padding: '$1 $2',
+      },
+      sm: {
+        fontSize: '$2',
+        padding: '$1 $2',
+      },
+      md: {
+        fontSize: '$3',
+        padding: '$2 $3',
+      },
+      lg: {
+        fontSize: '$4',
+        padding: '$2 $3',
+      },
     },
-  
-    '&:disabled': {
-      backgroundColor: '$gray6',
-      color: '$gray11',
+
+    variant: {
+      cancel: {
+        backgroundColor: '$gray2',
+        color: '$gray11',
+        '&:not([disabled])': {
+          border: '1px solid $gray8',
+        },
+      },
+      primary: {
+        backgroundColor: '$primary',
+        color: '$primaryText',
+        // '&:hover': {
+        //   backgroundColor: '$primaryLighten1',
+        // },
+      },
+      secondary: {
+        backgroundColor: '$secondary',
+        color: '$secondaryText',
+      },
     },
-    backgroundColor: '$primary',
-    color: '$primaryText',
-    fontSize: '$3',
-    padding: '$2 $3',
-   
-  })
+  },
+
+  defaultVariants: {
+    variant: DEFAULT_BUTTON_VARIANT,
+    size: DEFAULT_BUTTON_SIZE,
+  },
+})
 
 export function Button({children}: IButtonProps) {
     return (
--- src/lib/Button/types.ts
+++ src/lib/Button/types.ts
@@ -1,6 +1,14 @@
-import { PropsWithChildren } from "react";
+export const variantOptions = ['primary', 'secondary', 'cancel'] as const
+type Variant = typeof variantOptions[number]
+export const DEFAULT_BUTTON_VARIANT: Variant = 'primary'
 
-interface IButtonProps extends PropsWithChildren  {
+export const sizeOptions = ['xs', 'sm', 'md', 'lg'] as const
+type Size = typeof sizeOptions[number]
+export const DEFAULT_BUTTON_SIZE: Size = 'md'
+
+interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: Variant
+  size?: Size
 }
 
-export type {IButtonProps}
+export type { IButtonProps }
