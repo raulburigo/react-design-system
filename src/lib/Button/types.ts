680759273272bb7d5b9bd13ea729d866cbaf431d
Raul Burigo <raul.burigo@gmail.com>
2022-12-29 10:13:36 -0300

    update to v13

--- src/lib/Button/types.ts
+++ src/lib/Button/types.ts
@@ -1,10 +1,8 @@
 export const variantOptions = ['primary', 'secondary', 'cancel'] as const
 type Variant = typeof variantOptions[number]
-export const DEFAULT_BUTTON_VARIANT: Variant = 'primary'
 
 export const sizeOptions = ['xs', 'sm', 'md', 'lg'] as const
 type Size = typeof sizeOptions[number]
-export const DEFAULT_BUTTON_SIZE: Size = 'md'
 
 interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: Variant
