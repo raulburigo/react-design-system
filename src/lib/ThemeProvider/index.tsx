b674ddab950581f6f29ade3c257424f0ce804837
Raul Burigo <raul.burigo@gmail.com>
2023-01-06 08:27:10 -0300

    refactor: export default

--- src/lib/ThemeProvider/index.tsx
+++ src/lib/ThemeProvider/index.tsx
@@ -1,4 +1,28 @@
-import ThemeProvider from './ThemeProvider'
-export * from './ThemeProvider'
-export { ThemeProvider }
-export default ThemeProvider
+import React, { createContext, PropsWithChildren, useMemo } from 'react'
+import { createTheme, styled } from './stitches.config'
+import { IThemeProps } from './types'
+
+const ThemeContext = createContext({})
+
+const ThemeContainer = styled('div', {})
+
+export function ThemeProvider({
+  children,
+  colors: clientColors
+}: PropsWithChildren<IThemeProps>) {
+  const clientTheme = useMemo(
+    () =>
+      createTheme({
+        colors: {
+          ...clientColors
+        }
+      }),
+    [clientColors]
+  )
+
+  return (
+    <ThemeContext.Provider value={{}}>
+      <ThemeContainer className={clientTheme}>{children}</ThemeContainer>
+    </ThemeContext.Provider>
+  )
+}
